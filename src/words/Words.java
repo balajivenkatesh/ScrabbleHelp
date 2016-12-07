076743c6c043c5def05e3678f72c0a6ca3d6c738
Balaji <balaji.jagannathan@salesforce.com>
2016-12-07 02:36:08 -0800

    Minor formatting

--- src/words/Words.java
+++ src/words/Words.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 public interface Words {
 	public boolean isWord(String s);
+
 	public void indexWords();
+
 	public List<String> listWords();
+
 	public int getWordsCount();
 }
