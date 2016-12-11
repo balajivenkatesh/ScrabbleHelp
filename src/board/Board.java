3e08608a0b6290ac434724fecfb933219f015898
Balaji <balaji.jagannathan@salesforce.com>
2016-12-10 16:00:29 -0800

    Board fill word and best score move method

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,13 +1,107 @@
 package board;
 
+import java.util.Collection;
+
 import letter.Alphabet;
 
 public interface Board {
 
-	Alphabet[][] getGrid();
+	public int getSize();
 
-	void printBoard();
+	public Alphabet[][] getGrid();
 
-	void setTile(int i, int j, Alphabet a) throws Exception;
+	public void printBoard();
+
+	public void setTile(int i, int j, Alphabet a) throws Exception;
+
+	public FillWordResult fillWord(String s, int x, int y, boolean fillVert);
+
+	public FillWordResult putWord(String s, int x, int y, boolean fillVert);
+	
+	public Collection<Move> bestScoreMove(String s);
+
+	public enum FillWordResultState {
+		SUCCESS, ERROR;
+	}
+
+	public class FillWordResult {
+		private FillWordResultState state;
+		private int score;
+		private String msg;
+
+		public FillWordResult(FillWordResultState state, int score, String msg) {
+			this.state = state;
+			this.score = score;
+			this.msg = msg;
+		}
+
+		public static FillWordResult newFillWordError(String msg) {
+			return new FillWordResult(FillWordResultState.ERROR, 0, msg);
+		}
+
+		public static FillWordResult newFillWordSuccess(int score) {
+			return new FillWordResult(FillWordResultState.SUCCESS, score, "");
+		}
+
+		public FillWordResultState getState() {
+			return state;
+		}
+
+		public int getScore() {
+			return score;
+		}
+
+		public String getMsg() {
+			return msg;
+		}
+	}
+
+	public class Move implements Comparable<Move> {
+		private int x, y;
+		private boolean fillVert;
+		private int score;
+		private String word;
+		
+		public Move(String s, int i, int j, boolean fill, int p) {
+			x = i;
+			y = i;
+			fillVert = fill;
+			word = s;
+			score = p;
+		}
+
+		public int getX() {
+			return x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public boolean isFillVert() {
+			return fillVert;
+		}
+
+		public int getScore() {
+			return score;
+		}
+
+		public String getWord() {
+			return word;
+		}
+
+		@Override
+		public int compareTo(Move o) {
+			int aScore = score;
+			int bScore = o.getScore();
+			if (aScore > bScore) {
+				return 1;
+			} else if (aScore == bScore) {
+				return 0;
+			} else {
+				return -1;
+			}
+		}
+	}
 
 }
--- src/board/BoardImpl.java
+++ src/board/BoardImpl.java
@@ -1,10 +1,21 @@
 package board;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.PriorityQueue;
+
 import letter.Alphabet;
+import words.EngWords;
+import words.Words;
 
 public class BoardImpl implements Board {
 	private static final int side = 11;
 	private Alphabet grid[][] = new Alphabet[side][side];
+
+	private String horiz[][] = new String[side][side];
+	private String verti[][] = new String[side][side];
+
+	private Words engWords = new EngWords();
 
 	public BoardImpl() {
 		for (int i = 0; i < side; i++) {
@@ -27,6 +38,11 @@ public class BoardImpl implements Board {
 	}
 
 	@Override
+	public int getSize() {
+		return side;
+	}
+
+	@Override
 	public Alphabet[][] getGrid() {
 		return grid;
 	}
@@ -40,12 +56,191 @@ public class BoardImpl implements Board {
 			System.out.println();
 		}
 	}
-	
+
 	@Override
 	public void setTile(int i, int j, Alphabet a) throws Exception {
 		if (i >= side || j >= side) {
 			throw new Exception();
 		}
 		grid[i][j] = a;
+	}
+
+	@Override
+	public FillWordResult fillWord(String s, int x, int y, boolean fillVert) {
+		int n = s.length();
+
+		FillWordResult check = checkSizeBeforeFill(n, x, y, fillVert);
+		if (check != null) {
+			return check;
+		}
+
+		boolean touch = false;
+
+		// HashSet<String> resWords = new HashSet<>();
+		int sIndex = 0, i = x, j = y;
+		StringBuilder sb = new StringBuilder();
+		sb.append(getLeftNeighbour(x, y, fillVert));
+		while (sIndex < n && i < side && j < side) {
+			if (grid[i][j] == Alphabet.Empty) {
+				char c = s.charAt(sIndex);
+				sb.append(c);
+				sIndex++;
+				String perpenWord = getLeftNeighbour(i, j, !fillVert) + c + getRightNeighbour(i, j, !fillVert);
+				if (perpenWord.length() > 1) {
+					if (!engWords.isWord(perpenWord)) {
+						return FillWordResult.newFillWordError("No word - " + perpenWord);
+					} else {
+						touch = true;
+					}
+				}
+				// resWords.add(perpenWord);
+			} else {
+				sb.append(grid[i][j].getChar());
+			}
+			if (fillVert) {
+				i++;
+			} else {
+				j++;
+			}
+		}
+		if (sIndex != n) {
+			return FillWordResult.newFillWordError("Insufficient space");
+		}
+		sb.append(getWordAt(i, j, fillVert));
+
+		String mainWord = sb.toString();
+		if (mainWord.equals(s) && touch == false) {
+			return FillWordResult.newFillWordError("No touch");
+		}
+		if (!engWords.isWord(mainWord)) {
+			return FillWordResult.newFillWordError("No word - " + mainWord);
+		}
+		// resWords.add(sb.toString());
+		// Collection<String> invalid = engWords.isValidWords(resWords);
+		// if (invalid.isEmpty()) {
+		// return FillWordResult.SUCCESS;
+		// }
+
+		return FillWordResult.newFillWordSuccess(10);
+	}
+
+	@Override
+	public FillWordResult putWord(String s, int x, int y, boolean fillVert) {
+		int n = s.length();
+
+		FillWordResult check = checkSizeBeforeFill(n, x, y, fillVert);
+		if (check != null) {
+			return check;
+		}
+
+		ArrayList<Integer> iList = new ArrayList<>();
+		ArrayList<Integer> jList = new ArrayList<>();
+		int sIndex = 0, i = x, j = y;
+		while (sIndex < n && i < side && j < side) {
+			if (grid[i][j] == Alphabet.Empty) {
+				try {
+					grid[i][j] = Alphabet.getAlphabetEnum(s.charAt(sIndex));
+					iList.add(i);
+					jList.add(j);
+				} catch (Exception e) {
+					revertAtList(iList, jList);
+					return FillWordResult.newFillWordError("Invalid char");
+				}
+				sIndex++;
+			}
+			if (fillVert) {
+				i++;
+			} else {
+				j++;
+			}
+		}
+
+		if (sIndex != n) {
+			revertAtList(iList, jList);
+			return FillWordResult.newFillWordError("Insufficient space");
+		}
+
+		return FillWordResult.newFillWordSuccess(0);
+	}
+
+	@Override
+	public Collection<Move> bestScoreMove(String s) {
+		Collection<Move> bestMoves = new PriorityQueue<>(10);
+		bestScoreMove(s, true, bestMoves);
+		bestScoreMove(s, false, bestMoves);
+		return bestMoves;
+	}
+	
+	private void bestScoreMove(String s, boolean fillVert, Collection<Move> bestMoves) {
+		for (int i = 0; i < side; i++) {
+			for (int j = 0; j < side; j++) {
+				FillWordResult res = fillWord(s, i, j, fillVert);
+				if (res.getState() == FillWordResultState.SUCCESS) {
+					bestMoves.add(new Move(s, i, j, fillVert, res.getScore()));
+				}
+			}
+		}
+	}
+
+	private void revertAtList(ArrayList<Integer> iList, ArrayList<Integer> jList) {
+		for (int i = 0; i < iList.size(); i++) {
+			grid[iList.get(i)][jList.get(i)] = Alphabet.Empty;
+		}
+	}
+
+	private FillWordResult checkSizeBeforeFill(int len, int x, int y, boolean fillVert) {
+		if (grid[x][y] != Alphabet.Empty) {
+			return FillWordResult.newFillWordError("Non-empty start");
+		}
+
+		// Insufficient space
+		int init = x;
+		if (fillVert) {
+			init = y;
+		}
+		if (init + len > side) {
+			return FillWordResult.newFillWordError("Insufficient space before start");
+		}
+
+		return null;
+	}
+
+	private String getWordAt(int x, int y, boolean fillVert) {
+		if (fillVert) {
+			if (x < side) {
+				return verti[x][y];
+			}
+		} else {
+			if (y < side) {
+				return horiz[x][y];
+			}
+		}
+		return "";
+	}
+
+	private String getLeftNeighbour(int x, int y, boolean fillVert) {
+		if (fillVert) {
+			if (x > 0) {
+				return verti[x - 1][y];
+			}
+		} else {
+			if (y > 0) {
+				return horiz[x][y - 1];
+			}
+		}
+		return "";
+	}
+
+	private String getRightNeighbour(int x, int y, boolean fillVert) {
+		if (fillVert) {
+			if (x < side - 1) {
+				return verti[x + 1][y];
+			}
+		} else {
+			if (y < side - 1) {
+				return horiz[x][y + 1];
+			}
+		}
+		return "";
 	}
 }
